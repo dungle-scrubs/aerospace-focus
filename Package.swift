818bf91119b2a596a601ab5d01d73f59f5cd8aee
Kevin <dungle-scrubs@users.noreply.github.com>
2026-02-01 20:39:31 +0700

    Pin swift-argument-parser to 1.5.x for Swift 5.9 compatibility

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "aerospace-focus",
     platforms: [.macOS(.v14)],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.5.0")),
         .package(url: "https://github.com/LebJe/TOMLKit", from: "0.5.0"),
     ],
     targets: [
